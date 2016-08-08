71fdcf93d8f086faef255dbce409b95eea9a3b4e
Daniel J Raine <ley.danielj@gmail.com>
2016-08-07 20:14:47 -0400

    added matching logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
 var card1 = "queen";
 var card2 = "queen";
 var card3 = "king";
 var card4 = "king";
+
+if(card1 === card4) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again");
+};
