198fd388dd46f271472fb52bd9ea8cc86f2b4ee4
Daniel J Raine <ley.danielj@gmail.com>
2016-08-13 12:40:28 -0400

    Create div cards with JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,21 @@ var card2 = "queen";
 var card3 = "king";
 var card4 = "king";
 
-if(card1 === card4) {
-	alert("You found a match!");
+// if(card1 === card4) {
+// 	alert("You found a match!");
+// }
+// else {
+// 	alert("Sorry, try again");
+// };
+
+var createBoard = function(numOfCards) {
+	var gameBoard = document.getElementById('game-board');
+	var cardArray = [];
+	for (i = 1; i < numOfCards; i++) {
+		cardArray[i] = document.createElement('div');
+		cardArray[i].className = "card";
+		document.className('board').appendChild(cardArray[i]);
+	}	
 }
-else {
-	alert("Sorry, try again");
-};
+
+createBoard(4);
